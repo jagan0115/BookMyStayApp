bc01ef3daa3a1b94cf31b50f2542ece67da7371e
jagan0115 <jaganff15@gmail.com>
2026-04-01 10:44:23 +0530

    Jagan Added UC8-BookingHistory

---
+++ App/src/BookMyStayApp/BookingReportService.java
@@ -0,0 +1,14 @@
+package BookMyStayApp;
+
+public class BookingReportService {
+
+    public void generateReport(BookingHistory history) {
+
+        System.out.println("Booking History Report");
+
+        for (Reservation r : history.getConfirmedReservations()) {
+            System.out.println("Guest: " + r.getGuestName()
+                    + ", Room Type: " + r.getRoomType());
+        }
+    }
+}
---
+++ App/src/BookMyStayApp/UseCase8BookingHistoryReport.java
@@ -0,0 +1,19 @@
+package BookMyStayApp;
+
+public class UseCase8BookingHistoryReport {
+
+    public static void main(String[] args) {
+
+        System.out.println("Booking History and Reporting\n");
+
+        BookingHistory history = new BookingHistory();
+
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        BookingReportService reportService = new BookingReportService();
+
+        reportService.generateReport(history);
+    }
+}
