a5cb6c614a8fd5daf969ee5397ca704cad1558a3
jagan0115 <jaganff15@gmail.com>
2026-04-01 10:19:55 +0530

    Jagan Added UC4-RoomSearch

---
+++ App/src/BookMyStayApp/RoomSearchService.java
@@ -0,0 +1,33 @@
+package BookMyStayApp;
+
+import java.util.Map;
+
+public class RoomSearchService {
+
+    public void searchAvailableRooms(
+            RoomInventory inventory,
+            Room singleRoom,
+            Room doubleRoom,
+            Room suiteRoom) {
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        if (availability.get("Single") > 0) {
+            System.out.println("Single Room:");
+            singleRoom.displayRoomDetails();
+            System.out.println("Available: " + availability.get("Single") + "\n");
+        }
+
+        if (availability.get("Double") > 0) {
+            System.out.println("Double Room:");
+            doubleRoom.displayRoomDetails();
+            System.out.println("Available: " + availability.get("Double") + "\n");
+        }
+
+        if (availability.get("Suite") > 0) {
+            System.out.println("Suite Room:");
+            suiteRoom.displayRoomDetails();
+            System.out.println("Available: " + availability.get("Suite"));
+        }
+    }
+}
